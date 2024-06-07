a874d6356f32a35c325b77c6c1085e4b242e2239
Jason Wiker <jason.wiker@gmail.com>
2024-06-07 14:05:34 -0600

    Update index.tsx

--- packages/ui/src/header/index.tsx
+++ packages/ui/src/header/index.tsx
@@ -1,6 +1,6 @@
 export function Header({ name }: { name: string }): JSX.Element {
   return (
-    <div className="border-b-2 flex flex-row gap-4 justify-between py-6">
+    <div className="border-b-2 flex flex-row gap-4 justify-between py-8">
       <div className="flex flex-row gap-4">
         <a className="hover:underline" href="/">
           Home
